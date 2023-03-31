7baa23c32214c756dac399d162e77bfa52939931
Kayla Inabinet <>
2023-03-31 14:42:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let button=document.querySelector("button");
+let answer=document.querySelector("p");
+
+button.onclick=function(){
+    //write your code below using .innerHTML!
+  answer.innerHTML = ("Welcome Home Madea")
+};
